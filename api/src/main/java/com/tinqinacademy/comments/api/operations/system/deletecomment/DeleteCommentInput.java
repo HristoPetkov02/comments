36d21cf20fe05f52172e1409ec92e78ce9c3a6ea
LlamaLamap1 <hristo.petkov221@gmail.com>
2024-08-21 13:21:15 +0300

    Updated delete comment input.

--- api/src/main/java/com/tinqinacademy/comments/api/operations/system/deletecomment/DeleteCommentInput.java
+++ api/src/main/java/com/tinqinacademy/comments/api/operations/system/deletecomment/DeleteCommentInput.java
@@ -2,6 +2,7 @@ package com.tinqinacademy.comments.api.operations.system.deletecomment;
 
 import com.tinqinacademy.comments.api.base.OperationInput;
 import lombok.*;
+import org.hibernate.validator.constraints.UUID;
 
 @Getter
 @Setter
@@ -10,5 +11,6 @@ import lombok.*;
 @ToString
 @Builder
 public class DeleteCommentInput implements OperationInput {
+    @UUID(message = "Comment ID must be a valid UUID")
     private String commentId;
 }
